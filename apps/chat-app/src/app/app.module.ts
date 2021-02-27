ddcdb5eb42b8848bc88305c977262ff4a608c7b2
Jack Cao <khoicaominh.mmt@gmail.com>
2021-02-27 09:31:19 +0700

    chore: test grapql connection

--- apps/chat-app/src/app/app.module.ts
+++ apps/chat-app/src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql/graphql.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, HomeComponent],
     imports: [
         BrowserModule,
         HttpClientModule,
+        AppRoutingModule,
         GraphQLModule
     ],
     providers: [],
